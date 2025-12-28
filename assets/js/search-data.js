305d0aeb2e2d5315196d01188cf3a8fad003b208
RuiHuangNUS <ruihuang@nus.edu.sg>
2025-12-28 08:15:01 +0000

    Deploying to gh-pages from @ NUS-CORE/NUS-CORE.github.io@c68d15d97e13fcd87848be2f2b56202cf9695be9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-our-paper-titled-incremental-policy-iteration-for-unknown-nonlinear-systems-with-stability-and-performance-guarantees-has-been-accepted-by-ieee-cdc-2025-congrats-to-qingkai-and-fenglan-sparkles-smile",
+          title: 'Our paper titled “Incremental Policy Iteration for Unknown Nonlinear Systems with Stability and...',
+          description: "",
+          section: "News",},{id: "news-our-paper-titled-incremental-policy-iteration-for-unknown-nonlinear-systems-with-stability-and-performance-guarantees-has-been-accepted-by-ieee-cdc-2025-congrats-to-qingkai-and-fenglan-sparkles-smile",
+          title: 'Our paper titled “Incremental Policy Iteration for Unknown Nonlinear Systems with Stability and...',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
