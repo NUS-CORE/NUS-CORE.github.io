8a4f9345a4871192cb43b7b4507446d8ea4df24e
RuiHuangNUS <ruihuang@nus.edu.sg>
2025-12-28 08:25:36 +0000

    Deploying to gh-pages from @ NUS-CORE/NUS-CORE.github.io@0e3e9b92bfc1c24a7c019ea5555023ab4319244e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%7A%68%61%6F%6C%69%6E@%6E%75%73.%65%64%75.%73%67", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +515,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=091lFhYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
